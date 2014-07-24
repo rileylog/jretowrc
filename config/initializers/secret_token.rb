5ea669b1e87d4d2ccea208521d055f6fd0cf22e2
Logan Riley <rileylog@gmail.com>
2014-07-24 08:52:09 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jretowrc::Application.config.secret_token = '1d3a0e8f5dedac208fa0b8000f013f0f6a052c55d3f26e1fa9f028b925f7e70ff7d2f3c0a94d3d02c4ec02018c4e0c6d40ba245463690625eb7c1e51332cecbe'
